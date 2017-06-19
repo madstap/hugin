5464449ea2a9c490007ec0a35fff3b518b3a9cf6
Aleksander Madland Stapnes <madland.stapnes@gmail.com>
2017-06-18 23:56:05 -0300

    Release 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject madstap/hugin "0.1.0"
+(defproject madstap/hugin "0.1.1"
   :description "Some simple helpers for debugging."
   :url "https://github.com/madstap/hugin"
   :license {:name "Eclipse Public License"
